f8b31cc6db67daf8a4afbb49416ccf9702c71a4f
王子昂 <gnaizgnaw@163.com>
2019-06-25 20:24:11 +0800

    reformat test2

--- wza_src/ChineseNER/test/test2.py
+++ wza_src/ChineseNER/test/test2.py
@@ -1,36 +1,39 @@
 import os
+
+
 def test1():
-    filepath='E:\\foodtag\\652-1000\\'
-    pathDir = os.listdir(filepath)
-    for allDir in pathDir:
-        child = os.path.join('%s%s' % (filepath, allDir))
-        child0=allDir.split('.')[0]
-        print (child0)
+    filepath = 'E:\\foodtag\\652-1000\\'
+    path_dir = os.listdir(filepath)
+    for all_dir in path_dir:
+        child = os.path.join('%s%s' % (filepath, all_dir))
+        child0 = all_dir.split('.')[0]
+        print(child0)
         with open("E:\\foodtag\\foodTagTxtlast\\" + child0 + '.txt', 'w', encoding='utf-8') as fw:
-            with open(child,'r',encoding='utf-8') as fr:
-                anns=fr.readlines()
-                print (anns)
+            with open(child, 'r', encoding='utf-8') as fr:
+                anns = fr.readlines()
+                print(anns)
                 flag = True
-                index=0
-                i=1
+                index = 0
+                i = 1
                 for line in anns:
-                    if (line =="\n" or line.split()[0]=="O" ) and flag:
+                    if (line == "\n" or line.split()[0] == "O") and flag:
                         fw.write("\n")
                         flag = False
-                    elif (line =="\n" or line.split()[0]=="O" ) and flag==False:
+                    elif (line == "\n" or line.split()[0] == "O") and flag == False:
                         continue
                     else:
-                        if line.split()[0]=="B-Food":
-                            i=0
+                        if line.split()[0] == "B-Food":
+                            i = 0
                             continue
-                        if line.split()[0]=="I-Food":
+                        if line.split()[0] == "I-Food":
                             continue
-                        if i==0:
-                            fw.write(line.split()[0]+" "+"B-Food")
-                            flag=True
-                            i=i+1
+                        if i == 0:
+                            fw.write(line.split()[0] + " " + "B-Food")
+                            flag = True
+                            i = i + 1
                         else:
                             fw.write(line)
+
 
 def test2():
     filepath = 'E:\\foodtag\\foodTagTxtlast\\'
@@ -40,28 +43,31 @@ def test2():
         child0 = allDir.split('.')[0]
         import re
         childnum = re.findall('\d+', child0)
-        num=int(childnum[0])
-        if num<500:
+        num = int(childnum[0])
+        # 划分训练/测试/开发集
+        if num < 500:
             with open('food.train', 'a+', encoding='utf-8') as fw:
                 with open(child, 'r', encoding='utf-8') as fr:
-                        anns = fr.read()
+                    anns = fr.read()
                 fw.write(anns)
-        elif 500<=num<750:
+        elif 500 <= num < 750:
             with open('food.test', 'a+', encoding='utf-8') as fw:
                 with open(child, 'r', encoding='utf-8') as fr:
-                        anns = fr.read()
+                    anns = fr.read()
                 fw.write(anns)
-        elif num>700:
+        elif num > 700:
             with open('food.dev', 'a+', encoding='utf-8') as fw:
                 with open(child, 'r', encoding='utf-8') as fr:
-                        anns = fr.read()
+                    anns = fr.read()
                 fw.write(anns)
+
+
 def test3():
     filepath = 'E:\\foodtag\\652-1000txt\\'
-    pathDir = os.listdir(filepath)
-    for allDir in pathDir:
-        child = os.path.join('%s%s' % (filepath, allDir))
-        child0 = allDir.split('.')[0]
+    path_dir = os.listdir(filepath)
+    for all_dir in path_dir:
+        child = os.path.join('%s%s' % (filepath, all_dir))
+        child0 = all_dir.split('.')[0]
         print(child0)
         with open("E:\\foodtag\\FoodTagNew\\" + child0 + '.txt', 'w', encoding='utf-8') as fw:
             with open(child, 'r', encoding='utf-8') as fr:
@@ -70,7 +76,7 @@ def test3():
                 flag = True
                 index = 0
                 for line in anns:
-                    if index==0:
+                    if index == 0:
                         index += 1
                         continue
                     else:
@@ -78,5 +84,5 @@ def test3():
                         index += 1
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     test2()
