6aacb2f3715fed49607e76a94209ce9780574d34
王子昂 <gnaizgnaw@163.com>
2019-06-26 11:46:02 +0800

    update code

--- dn_src/ldatest/src/word2vec.py
+++ dn_src/ldatest/src/word2vec.py
@@ -1,9 +1,13 @@
 from os import listdir
-from gensim.models import Word2Vec
-from gensim.models.word2vec import PathLineSentences
+
 import multiprocessing
 import os
 import gensim
+from gensim import utils  # utility fnc for pickling, common scipy operations etc
+from gensim.models.word2vec import Word2Vec
+from gensim.models.word2vec import PathLineSentences
+import itertools
+import logging
 
 """
 训练 并保存Doc2Vec 模型
@@ -15,24 +19,20 @@ def train():
     # 先把所有文档的路径存进一个 array中，docLabels：
     input_dir = "../txt/topic_doc_pk"
     # 训练 Doc2Vec，并保存模型：
-    gensim.models
     # 特征向量维度：256；窗口大小：5；训练并行数；迭代次数
     model = Word2Vec(PathLineSentences(input_dir), size=256, window=10, min_count=5,
                      workers=multiprocessing.cpu_count(), iter=10)
-    outp1 = "../word2vec_models/shipin.model"
-    outp2 = "../word2vec_models/shipin.txt"
-    model.save(outp1)
-    model.similarity()
-    model.wv.save_word2vec_format(outp2, binary=False)
+    output_model = "../word2vec_models/food_word_vector.model"
+    output_txt = "../word2vec_models/good_word_vector.txt"
+    model.save(output_model)
+    model.wv.save_word2vec_format(output_txt, binary=False)
     print("model saved")
+    # model.similarity(u'瘦肉精', u'地沟油')
 
 
-import logging
-
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(
+    __name__)
 MAX_WORDS_IN_BATCH = 10000
-from gensim import utils, matutils  # utility fnc for pickling, common scipy operations etc
-import itertools
 
 """
 sentence迭代器
@@ -67,9 +67,9 @@ class PathLineSentences(object):
             # 存储目录下的所有文件（排序）
             self.input_files = []
             # 提取目录下的所有文件
-            for fpathe, dirs, fs in os.walk(self.source):
-                for f in fs:
-                    self.input_files.append(os.path.join(fpathe, f))
+            for root, dirs, files in os.walk(self.source):
+                for f in files:
+                    self.input_files.append(os.path.join(root, f))
             self.input_files.sort()  # makes sure it happens in filename order
         else:  # not a file or a directory, then we can't do anything with it
             raise ValueError('input is neither a file nor a path')
@@ -85,7 +85,7 @@ class PathLineSentences(object):
                 # 获得文件中的一行
                 for line in itertools.islice(fin, self.limit):
                     # 将一行内容（分词结果）以空格切分为链表
-                    line = utils.to_unicode(line).split()
+                    line = str(line, encoding='utf-8').split()
                     i = 0
                     while i < len(line):
                         # 分次返回指定长度的句子的分词列表
@@ -93,9 +93,9 @@ class PathLineSentences(object):
                         i += self.max_sentence_length
 
 
-def usemodel():
+def use_model():
     model = gensim.models.KeyedVectors.load_word2vec_format('../corpus/sgns.renmin.word', binary=False)
-    print(model.get_vector('十分愤怒'))
+    print(model.get_vector('瘦肉精'))
     sim = model.most_similar(u'瘦肉精', topn=10)
     print('\n瘦肉精-top10:')
     for item in sim:
@@ -104,11 +104,12 @@ def usemodel():
     print('\n地沟油-top10:')
     for item in sim2:
         print(item[0], item[1])
-    sim3 = model.most_similar(u'非洲猪瘟', topn=10)
+    sim3 = model.most_similar(u'猪瘟', topn=10)
     print('\n非洲猪瘟-top10:')
     for item in sim3:
         print(item[0], item[1])
 
 
 if __name__ == '__main__':
-    usemodel()
+    # train()
+    use_model()
