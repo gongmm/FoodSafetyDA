d71b09c544667f02460aebcf86030370dc93b52e
Yiner <yulianwei1998@163.com>
2019-06-28 16:35:18 +0800

    添加txt转csv代码

---
+++ yiner_src/txt2csv.py
@@ -0,0 +1,40 @@
+import csv
+import os
+
+def txt2csv(src_dir, des_dir, csv_name):
+    if src_dir not in os.listdir():
+        print('文件夹%s不存在' %src_dir)
+        return
+    txt_list = os.listdir(src_dir)
+    if '.DS_Store' in txt_list:
+        index = txt_list.index('.DS_Store')
+        txt_list.pop(index)
+    if len(txt_list) == 0:
+        print('没有txt文本')
+        return
+
+    rows = []
+
+    for txt in txt_list:
+        path = os.path.join(src_dir, txt)
+        with open(path, 'r') as f:
+            content = f.read()
+        row = [txt[:-4], content]
+        rows.append(row)
+    headers = ['topic', 'content']
+
+
+
+    if des_dir not in os.listdir():
+        os.mkdir(des_dir)
+    path = os.path.join(des_dir, csv_name)
+    with open(path, 'w') as f:
+        f_csv = csv_writer(f)
+        f_csv.writerow(headers)
+        f_csv.writerows(rows)
+
+if __name__ == '__main__':
+    src_dir = 'words_result/'
+    des_dir = 'csv/'
+    csv_name = 'cctv_news.csv'
+    txt2csv(src_dir, des_dir, csv_name)
