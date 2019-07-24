7caaf4cae789527762fd5ceae6b9745f0db70dca
王子昂 <gnaizgnaw@163.com>
2019-07-24 15:48:13 +0800

    添加IP Judge项目 判断IP是否可用

---
+++ wza_src/IPJudge/ProxyJudge.py
@@ -0,0 +1,40 @@
+import requests
+
+
+def get_ip():
+    proxy_list = []
+    with open('proxy.txt', 'r') as f:
+        for line in f.readlines():
+            proxy_list.append(line.strip('\n'))
+    return proxy_list
+
+
+def judge_ip(ip):
+    #  判断ip 是否可用
+    # http_url = 'http://news.foodmate.net'
+    # http_url = 'https://blog.csdn.net/sixkery/article/details/82726177'
+    http_url = 'https://www.baidu.com'
+    # http_url = 'http://www.eshian.com'
+    proxy_url = 'http://{0}'.format(ip)
+    print("Testing: " + proxy_url)
+    try:
+        proxy_dict = {
+            'http': proxy_url
+        }
+        r = requests.get(http_url, proxies=proxy_dict)
+        print('effective ip')
+        if 200 <= r.status_code < 300:
+            print('effective ip')
+            return True
+        else:
+            print('invalid ip and port 无效')
+            return False
+    except Exception as e:
+        print(str(e))
+        return False
+
+
+if __name__ == '__main__':
+    list = get_ip()
+    for ip in list:
+        judge_ip(ip)
