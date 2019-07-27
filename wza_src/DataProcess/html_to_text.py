aee015ddac3537321ed032635dc50de95170932d
王子昂 <gnaizgnaw@163.com>
2019-07-27 16:23:20 +0800

    处理wechat数据中的html标签

---
+++ wza_src/DataProcess/html_to_text.py
@@ -0,0 +1,18 @@
+import csv
+import re
+
+
+def html_to_text(readfile, writefile):
+    with open(readfile, 'r', encoding='gbk') as f:
+        with open(writefile, 'w', encoding='gbk', newline='') as f1:
+            rows = csv.reader(f)
+            writer = csv.writer(f1)
+            for row in rows:
+                content_without_tag = re.sub(r'<.*?>', '', row[-1])
+                row[-1] = content_without_tag.replace('\\n', '').strip()
+                print(row)
+                writer.writerow(row)
+
+
+if __name__ == '__main__':
+    html_to_text('wechat.csv', 'wechat_new.csv')
