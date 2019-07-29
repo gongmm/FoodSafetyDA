4295195349def943dd9d2d919bed855699610a7c
王子昂 <gnaizgnaw@163.com>
2019-07-30 00:47:59 +0800

    添加gensim LDA对比方法 重构代码

---
+++ wza_src/DataProcess/GensimLDATopicAnalysis.py
@@ -0,0 +1,42 @@
+import codecs
+from time import time
+from gensim import corpora
+from gensim.models import LdaModel
+from gensim import models
+from gensim.corpora import Dictionary
+
+corpus = []
+n_topic = 36
+corpus_file = 'corpus/news_content_corpus.txt'
+lda_model_file = 'model/lda_sk.model'
+feature_names_model_file = 'model/features_sk.model'
+doc_topic_dist_file = 'model/doc_topic_sk.model'
+n_top_words = 20  # 选取的关键词个数
+sk_doc_topic_file = 'model/sk_doc_topic.model'
+
+
+def get_corpus():
+    """ 读取txt文件里面的内容建立语料库"""
+    with open(corpus_file, 'r', encoding='UTF-8') as file:
+        for line in file.readlines():
+            corpus.append(line.strip())
+    print(len(corpus))
+
+
+def gensim_lda_train():
+    print("Loading dataset...")
+    t0 = time()
+    get_corpus()
+    print("done in %0.3fs." % (time() - t0))
+    dictionary = corpora.Dictionary(corpus)
+    common_corpus = [dictionary.doc2bow(text) for text in corpus]
+    tfidf = models.TfidfModel(common_corpus)
+    corpus_tfidf = tfidf[common_corpus]
+    # Run the LDA model for XX topics
+    lda = LdaModel(corpus=corpus_tfidf, id2word=dictionary, num_topics=n_topic, passes=10)
+    doc_topic = [a for a in lda[corpus]]
+    topics_r = lda.print_topics(num_topics=n_topic, num_words=n_top_words)
+
+
+if __name__ == '__main__':
+    gensim_lda_train()
--- wza_src/DataProcess/LDAClass.py
+++ wza_src/DataProcess/LDAClass.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 import csv
-from sklearn.decomposition import LatentDirichletAllocation
-from sklearn.cluster import KMeans
-from time import time
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.externals import joblib
-from sklearn import metrics
 import numpy as np
 import lda
 import sys
+import matplotlib.pyplot as plt
 
 
 class LDAClass:
 
     def __init__(self, n_topic=36, corpus_file='corpus/news_content_corpus.txt',
                  lda_model_file='model/lda.model',
-                 feature_names_model_file='model/feature.model',
-                 doc_topic_dist_file='model/sk_doc_topic.model'):
+                 feature_names_model_file='model/feature.model'):
         self.corpus = []
         self.corpus_file = corpus_file
         self.lda_model_file = lda_model_file
         self.feature_names_model_file = feature_names_model_file
         self.n_topic = n_topic
-        self.sk_doc_topic_file = doc_topic_dist_file
 
     def get_corpus(self):
         """ 读取txt文件里面的内容建立语料库"""
@@ -41,74 +35,14 @@ class LDAClass:
             n_top_words : 选取的关键词个数
         """
         lda_model = joblib.load(self.lda_model_file)
-        doc_topic_dist = joblib.load(self.sk_doc_topic_file)
-        tf_feature_names = joblib.load(self.feature_names_model_file)
-
         if lda_model is None:
             return
         # 主题-词分布
-        # topic_word = lda_model.topic_word_
-        for topic_idx, topic in enumerate(lda_model.components_):
-            print("Topic #%d:" % topic_idx)
-            print(" ".join([tf_feature_names[i]
-                            for i in topic.argsort()[:-n_top_words - 1:-1]]))
-
-        print("type(doc_topic):{}".format(type(doc_topic_dist)))
-        print("shape:{}".format(doc_topic_dist.shape))
-        with open('all_news_data_utf.csv', 'r', encoding='utf-8') as f_read:
-            rows = csv.reader(f_read)
-            with open('result/all_news_data_utf_topic_sk.csv', 'w', encoding='utf-8', newline='') as f_write:
-                writer = csv.writer(f_write)
-                n = 0
-                for row in rows:
-                    if n == 0:
-                        topic_most_pr = 'topic_id'
-                        row.append(topic_most_pr)
-                        # 添加行
-                        writer.writerow(row)
-                    # elif n <= 10000:
-                    else:
-                        # 选择最可能的主题，对文档进行标记
-                        topic_most_pr = doc_topic_dist[n - 1].argmax()
-                        row.append(topic_most_pr)
-                        writer.writerow(row)
-                    n = n + 1
-
-    def lda_kmeans(self):
-        """lda+kmeans"""
-        # 将文本转为词频矩阵
-        # 创建词袋数据结构
-        cnt_vector = CountVectorizer(max_features=1000)
-        # 得到元素--词频
-        cnt_tf = cnt_vector.fit_transform(self.corpus)
-        # 所有的特征词，即关键词
-
-        self.cnt_feature = cnt_vector.get_feature_names()
-        transformer = TfidfTransformer()
-        tf_idf = transformer.fit_transform(cnt_tf)
-        tf_idfweight = tf_idf.toarray()
-        t0 = time()
-        # lda主题聚类
-        lda = LatentDirichletAllocation(
-            learning_offset=50.,
-            random_state=0, max_iter=1000)
-        lda.fit(cnt_tf)
-        perplexity = lda.perplexity(X=cnt_tf, sub_sampling=False)
-        # 查看lda困惑度
-        print(perplexity)
-        # 求出文档-主题分布
-        doc_res = lda.fit_transform(cnt_tf)
-        self.print_top_words()  # 打印主题词
-        '''聚类，设置K的个数以及质心'''
-        kmeans_model = KMeans(n_clusters=111, init='k-means++')
-        y_pred = kmeans_model.fit_predict(doc_res)
-        print(y_pred)
-
-        labels = kmeans_model.labels_
-        # 求其轮廓系数，轮廓系数越大 聚类效果越好
-        silhouette = metrics.silhouette_score(doc_res, labels, metric='euclidean')
-        print(silhouette)
-        print("done in %0.3fs." % (time() - t0))  # 运行的时间
+        topic_word = lda_model.topic_word_
+        word = joblib.load(self.feature_names_model_file)
+        for i, topic_dist in enumerate(topic_word):
+            topic_words = np.array(word)[np.argsort(topic_dist)][:-(n_top_words + 1):-1]
+            print(u'*Topic {}\n- {}'.format(i, ' '.join(topic_words)))
 
     def save_topic(self, writefile):
         """ 将每篇doc对应的topic存储 [doc_id, topic_id]
@@ -172,6 +106,46 @@ class LDAClass:
                         writer.writerow(row)
                     n = n + 1
 
+    def draw_topic_word(self):
+        lda_model = joblib.load(self.lda_model_file)
+        if lda_model is None:
+            return
+        # 主题-词分布
+        topic_word = lda_model.topic_word_
+        f, ax = plt.subplots(5, 1, figsize=(8, 6))
+        for i, k in enumerate([1, 5, 9, 14, 19]):
+            ax[i].stem(topic_word[k, :], linefmt='b-',
+                       markerfmt='bo', basefmt='w-')
+            ax[i].set_xlim(-50, 4350)
+            ax[i].set_ylim(0, 0.08)
+            ax[i].set_ylabel("Prob")
+            ax[i].set_title("topic {}".format(k))
+
+        ax[4].set_xlabel("word")
+
+        plt.tight_layout()
+        plt.show()
+
+    def draw_doc_topic(self):
+        lda_model = joblib.load(self.lda_model_file)
+        if lda_model is None:
+            return
+        # 文档-主题分布 doc_topic
+        doc_topic = lda_model.doc_topic_
+        f, ax = plt.subplots(5, 1, figsize=(8, 6))
+        for i, k in enumerate([1, 3, 4, 8, 9]):
+            ax[i].stem(doc_topic[k, :], linefmt='r-',
+                       markerfmt='ro', basefmt='w-')
+            ax[i].set_xlim(0, self.n_topic)
+            ax[i].set_ylim(0, 1)
+            ax[i].set_ylabel("Prob")
+            ax[i].set_title("Document {}".format(k))
+
+        ax[4].set_xlabel("Topic")
+
+        plt.tight_layout()
+        plt.show()
+
     def save_topic_word(self, n_top_words=20, writefile='result/food_topic_word.csv'):
         """将话题关键词存入单独的csv文件 ['topic_id', 'topic_word']
 
@@ -191,45 +165,6 @@ class LDAClass:
             for i, topic_dist in enumerate(topic_word):
                 topic_words = np.array(word)[np.argsort(topic_dist)][:-(n_top_words + 1):-1]
                 csv_writer.writerow([i, ' '.join(topic_words)])
-                print(u'*Topic {}\n- {}'.format(i, ' '.join(topic_words)))
-
-    def sklearn_lda(self):
-        """利用 sklearn 中的 lda 模型进行训练"""
-        print("======生成语料=====")
-        self.get_corpus()
-        print("Extracting tf features for LDA...")
-        tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
-                                        max_features=2000,
-                                        stop_words='english')
-        t0 = time()
-        tf = tf_vectorizer.fit_transform(self.corpus)
-        print("done in %0.3fs." % (time() - t0))
-        print()
-        print("======开始sklean_LDA=====")
-        lda = LatentDirichletAllocation(n_components=self.n_topic, max_iter=5,
-                                        learning_method='online',
-                                        learning_offset=50.,
-                                        random_state=0)
-        t0 = time()
-        lda.fit(tf)
-        print("done in %0.3fs." % (time() - t0))
-
-        print("\nTopics in LDA model:")
-        tf_feature_names = tf_vectorizer.get_feature_names()
-        joblib.dump(tf_feature_names, self.feature_names_model_file)
-        for topic_idx, topic in enumerate(lda.components_):
-            message = "Topic #%d: " % topic_idx
-            message += " ".join([tf_feature_names[i]
-                                 for i in topic.argsort()[:-self.n_topic - 1:-1]])
-            print(message)
-        print()
-
-        t0 = time()
-        doc_topic_dist = lda.transform(tf)
-        joblib.dump(lda, self.lda_model_file)
-        joblib.dump(doc_topic_dist, self.sk_doc_topic_file)
-        print("done in %0.3fs." % (time() - t0))
-        print("lda perplexity %.3f" % lda.perplexity(tf))
 
     def train(self):
         """lda train"""
@@ -257,17 +192,13 @@ if __name__ == '__main__':
     # lda_class = LDAClass(corpus_file='corpus/food_news_corpus.txt')
     lda_class = LDAClass()
     if len(sys.argv) > 1:
-        if sys.argv[-1] == 'sk':
-            lda_class = LDAClass(lda_model_file='model/lda_sk.model',
-                                 feature_names_model_file='model/feature_sk.model')
-            # 训练LDA模型
-            # lda_class.sklearn_lda()
-        else:
-            lda_class.train()
+        lda_class.train()
 
     # 获得话题的对应特征词
     # lda_class.save_topic_word()
     lda_class.print_top_words()
     # 在新闻文档中添加话题标签
-    # lda_class.save_topic('result/doc_topic.csv')
+    lda_class.save_topic('result/doc_topic.csv')
+    lda_class.draw_doc_topic()
+    # lda_class.draw_topic_word()
     # lda_class.write_doc_topic_to_origin('all_news_data_utf.csv', 'result/all_news_data_utf_topic.csv')
