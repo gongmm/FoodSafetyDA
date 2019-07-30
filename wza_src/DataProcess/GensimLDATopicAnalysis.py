a7bbb9c1f7a065eedfe21585e97c3262d548eb8d
王子昂 <gnaizgnaw@163.com>
2019-07-30 11:58:36 +0800

    修改

--- wza_src/DataProcess/GensimLDATopicAnalysis.py
+++ wza_src/DataProcess/GensimLDATopicAnalysis.py
@@ -4,9 +4,9 @@ from gensim import corpora
 from gensim.models import LdaModel
 from gensim import models
 from gensim.corpora import Dictionary
+import sys
 from sklearn.externals import joblib
 
-corpus = []
 n_topic = 36
 corpus_file = 'corpus/news_content_corpus.txt'
 lda_model_file = 'model/lda_gensim.model'
@@ -18,22 +18,26 @@ sk_doc_topic_file = 'model/gensim_doc_topic.model'
 
 def get_corpus():
     """ 读取txt文件里面的内容建立语料库"""
+    corpus = []
     with open(corpus_file, 'r', encoding='UTF-8') as file:
         for line in file.readlines():
             corpus.append(line.strip().split())
+            # corpus.append([line.strip()])
     print(len(corpus))
-
-
-def gensim_lda_train():
-    print("Loading dataset...")
-    t0 = time()
-    get_corpus()
-    print("done in %0.3fs." % (time() - t0))
     print("To Dictionary...")
     t0 = time()
     dictionary = corpora.Dictionary(corpus)
     common_corpus = [dictionary.doc2bow(text) for text in corpus]
     print("done in %0.3fs." % (time() - t0))
+    return dictionary, common_corpus
+
+
+def gensim_lda_train():
+    print("Loading dataset...")
+    t0 = time()
+    dictionary, common_corpus = get_corpus()
+    print("done in %0.3fs." % (time() - t0))
+
     print("TF-IDF...")
     t0 = time()
     tfidf = models.TfidfModel(common_corpus)
@@ -48,5 +52,18 @@ def gensim_lda_train():
     # topics_r = lda.print_topics(num_topics=n_topic, num_words=n_top_words)
 
 
+def print_info():
+    dictionary, common_corpus = get_corpus()
+    lda = joblib.load(lda_model_file)
+    doc_topic = lda.get_document_topics(common_corpus)
+    topics = lda.print_topics(num_topics=n_topic, num_words=n_top_words)
+    print(doc_topic)
+    for topic in topics:
+        print(topic)
+    print(topics)
+
+
 if __name__ == '__main__':
-    gensim_lda_train()
+    if len(sys.argv) > 1:
+        gensim_lda_train()
+    print_info()
