9c98dd94cb16be93fc3c3897f1007de8a6e6802c
gongmm <gnaizgnaw@163.com>
2019-08-02 17:33:54 +0800

    delete corpus add word2vec

--- wza_src/DataProcess/Word2VecModel.py
+++ wza_src/DataProcess/Word2VecModel.py
@@ -7,13 +7,14 @@ from sklearn.externals import joblib
 
 corpus = []
 word2vec_model_file = 'model/word2vec.model'
+corpus_file='corpus/news_content_corpus.txt'
 
 
-def get_corpus(corpus_file='corpus/news_content_corpus.txt'):
+def get_corpus():
     """ 读取txt文件里面的内容建立语料库"""
     with open(corpus_file, 'r', encoding='UTF-8') as file:
         for line in file.readlines():
-            corpus.append(line.strip())
+            corpus.append([line.strip()])
     print(len(corpus))
     return corpus
 
@@ -21,7 +22,8 @@ def get_corpus(corpus_file='corpus/news_content_corpus.txt'):
 def train():
     corpus = get_corpus()
     # 训练模型，部分参数如下
-    model = word2vec.Word2Vec(corpus, size=100, hs=1, min_count=1, window=3)
+    sentences = word2vec.Text8Corpus(corpus_file)
+    model = word2vec.Word2Vec(sentences, size=100, hs=1, min_count=1, window=3)
     model.save(word2vec_model_file)
 
 
@@ -32,14 +34,15 @@ def evaluate():
 
     # 计算两个词向量的相似度
     try:
-        sim1 = model.similarity(u'中央企业', u'事业单位')
-        sim2 = model.similarity(u'教育网', u'新闻网')
+        sim1 = model.similarity(u'黑作坊', u'小作坊')
+        sim2 = model.similarity(u'猪瘟', u'瘦肉精')
     except KeyError:
         sim1 = 0
         sim2 = 0
-    print(u'中央企业 和 事业单位 的相似度为 ', sim1)
-    print(u'人民教育网 和 新闻网 的相似度为 ', sim2)
+    print(u'黑作坊 和 小作坊 的相似度为 ', sim1)
+    print(u'猪瘟 和 瘦肉精 的相似度为 ', sim2)
 
 
 if __name__ == '__main__':
-    train()
+    # train()
+    evaluate()
