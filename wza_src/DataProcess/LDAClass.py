049c5254b8e98a597d587e4a0db94fb92ffb95a3
王子昂 <gnaizgnaw@163.com>
2019-08-03 10:58:44 +0800

    重新训练LDA模型，更换主题数

--- wza_src/DataProcess/LDAClass.py
+++ wza_src/DataProcess/LDAClass.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 class LDAClass:
 
-    def __init__(self, n_topic=36, corpus_file='corpus/news_content_corpus.txt',
+    def __init__(self, n_topic=45, corpus_file='corpus/news_content_corpus.txt',
                  lda_model_file='model/lda.model',
                  feature_names_model_file='model/feature.model'):
         self.corpus = []
@@ -200,10 +200,10 @@ if __name__ == '__main__':
         lda_class.train()
 
     # 获得话题的对应特征词
-    # lda_class.save_topic_word()
+    lda_class.save_topic_word()
     lda_class.print_top_words()
     # 在新闻文档中添加话题标签
     lda_class.save_topic()
     lda_class.draw_doc_topic()
-    # lda_class.draw_topic_word()
-    # lda_class.write_doc_topic_to_origin('all_news_data_utf.csv', 'result/all_news_data_utf_topic.csv')
+    lda_class.draw_topic_word()
+    lda_class.write_doc_topic_to_origin('all_news_data_utf.csv', 'result/all_news_data_utf_topic.csv')
