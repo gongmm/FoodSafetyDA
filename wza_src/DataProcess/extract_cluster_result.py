cbbcb2aa127cdc80199ae119571cdde72a4d114b
王子昂 <gnaizgnaw@163.com>
2019-08-05 10:38:54 +0800

    修改文件名 添加注释

--- wza_src/DataProcess/extract_cluster_result.py
+++ wza_src/DataProcess/extract_cluster_result.py
@@ -9,7 +9,11 @@ topic_doc_dir = 'topic_doc'
 
 
 def get_cluster_result():
-    """获得层次聚类后的结果"""
+    """
+    获得层次聚类后的结果
+    Returns:
+
+    """
     print('————开始存储结果————')
     cluster_result_dir = os.path.join('cluster', 'result')
     is_exists = os.path.exists(cluster_result_dir)
@@ -23,12 +27,21 @@ def get_cluster_result():
 
 
 def get_cluster_topic_result(matrix_file, topic_index):
+    """
+    获得某个主题的聚类结果
+    Args:
+        matrix_file: 矩阵文件名
+        topic_index: 处理的话题号
+
+    Returns:
+        主题中各个事件包括的文档名，二维链表(event_number, doc_number)
+    """
     print('————处理第' + topic_index + '个主题————')
     cluster_results = []
-    # 存结果矩阵
+    # 结果矩阵
     cluster_file = os.path.join(cluster_matrix_dir, matrix_file)
     matrix = joblib.load(cluster_file)
-    # 存文档索引
+    # 文档索引
     doc_id_file = os.path.join(vec_dir, 'topic' + topic_index + '_doc.index')
     doc_index = joblib.load(doc_id_file)
     # 聚类个数
@@ -41,19 +54,20 @@ def get_cluster_topic_result(matrix_file, topic_index):
             doc_name = 'topic' + topic_index + '_doc' + str(doc_num) + '.txt'
             cluster_result.append(doc_name)
         cluster_results.append(cluster_result)
-    # pd_matrix = pd.DataFrame(matrix, columns=['cluster_id'])
-    # result_matrix_file = os.path.join(cluster_result_dir, 'topic' + index + '_matrix.csv')
-    # pd_matrix.to_csv(result_matrix_file, index=False)
-
-    # pd_index = pd.DataFrame(doc_index)
-    # result_index_file = os.path.join(cluster_result_dir, 'topic' + index + '_index.csv')
-    # pd_index.to_csv(result_index_file, index=True)
-
     print('————结束存储结果————')
     return cluster_results
 
 
 def save_topic_cluster_result(topic_index, cluster_topic_result):
+    """
+    保存聚类结果，将主题下的文档按照事件分类存储 topic_i/events/event_j/doc...
+    Args:
+        topic_index: 处理的话题号
+        cluster_topic_result: 主题的聚类结果
+
+    Returns:
+
+    """
     topic_dir = os.path.join(topic_doc_dir, 'topic' + topic_index)
     # 如果不存在则创建目录
     if not os.path.exists(topic_dir):
--- wza_src/DataProcess/extract_keyword.py
+++ wza_src/DataProcess/extract_keyword.py
@@ -3,6 +3,12 @@ import os
 
 
 def get_all_files(path):
+    """
+    获得路径下的所有文件，包括子文件夹下的文件
+
+    Args:
+        path :  目标文件夹的路径
+    """
     files_ = []
     dir_list = os.listdir(path)
     for i in range(0, len(dir_list)):
@@ -15,6 +21,16 @@ def get_all_files(path):
 
 
 def extract_single_doc(content):
+    """
+    提取文本中的前十个关键词
+
+    Args:
+        content: 文本内容
+
+    Returns:
+        前十个关键词 list
+
+    """
     keywords = []
     # for keyword, weight in extract_tags(content, withWeight=True):
     #     keywords.append(keyword)
@@ -26,6 +42,15 @@ def extract_single_doc(content):
 
 
 def extract_event(path):
+    """
+    提取一个事件中的关键词，将描述该事件的文档拼接然后进行关键词提取
+    Args:
+        path: 事件文件夹所在的路径
+
+    Returns:
+        事件的前十个关键词
+
+    """
     content = ''
     files = get_all_files(path)
     for file in files:
@@ -36,6 +61,14 @@ def extract_event(path):
 
 
 def extract_topic(path):
+    """
+    提取话题中所有事件的关键词
+    Args:
+        path: 话题文件夹所在的路径
+
+    Returns:
+
+    """
     topic_keywords = []
     events_path = os.path.join(path, 'events')
     events = os.listdir(events_path)
@@ -46,6 +79,15 @@ def extract_topic(path):
 
 
 def save_keywords(path, topic_keywords):
+    """
+    保存话题中各个事件的关键词
+    Args:
+        path: 话题文件夹所在的路径
+        topic_keywords: 话题中各个事件的关键词列表
+
+    Returns:
+
+    """
     events_keyword_dir = 'events_keyword'
     save_path = os.path.join(path, events_keyword_dir)
     # 如果不存在则创建目录
