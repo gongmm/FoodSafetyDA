73a91b4bb6c35366d3d55e3bcfb07bc7aaef5178
YINNNER <yulianwei1998@gmail.com>
2019-08-12 14:36:27 +0800

    添加注释

--- src/EventsDistribution/create_map.py
+++ src/EventsDistribution/create_map.py
@@ -13,6 +13,16 @@ dic_file = 'province_dic_list'
 
 
 def create_map(topic_num):
+    """为每个主题分别绘制热度图
+
+    首先调用get_location函数获得每个话题的省份数据，
+    再调用create_map_core函数添加数据到地图中，
+    最后调用render函数渲染地图，真正绘制到html文件中。
+    将所有主题的省份数据存储在文件中，供后续绘制包含所有主题数据的热度图使用。
+
+    Args:
+        topic_num: 主题的数量
+    """
     province_dic_list = []
     for i in range(topic_num):
         province_distribution = get_location(i)
@@ -27,6 +37,10 @@ def create_map(topic_num):
 
 
 def create_total_map():
+    """绘制包含所有主题数据的热度图
+
+    从文件中读入所有主题的省份数据。
+    """
     province_dic_list = joblib.load(dic_file)
     n = len(province_dic_list)
     name_list = ['topic'+str(i) for i in range(n)]
@@ -36,6 +50,15 @@ def create_total_map():
 
 
 def create_map_core(name, dic):
+    """为地图添加一种主题的数据
+
+    Args:
+        name: 数据系列的名称
+        dic: 主题省份数据
+
+    Returns:
+        china_map: 地图
+    """
     max_value = max(dic.values())
     china_map = map_config(max_value)
     china_map.add(name, [list(z) for z in zip(dic.keys(), dic.values())], 'china', is_roam=False, zoom=1.1)
@@ -43,6 +66,15 @@ def create_map_core(name, dic):
 
 
 def create_map_list_core(name_list, dic_list):
+    """为地图添加所有主题的数据
+
+    Args:
+        name_list: 数据系列的名称列表
+        dic_list: 主题省份数据列表
+
+    Returns:
+        china_map: 地图
+    """
     # 字典合并过程会改变第一个字典，因此需要备份再替换
     first_dic = dic_list[0].copy()
     total_dic = merge_all_dict(dic_list)
@@ -66,6 +98,23 @@ def create_map_list_core(name_list, dic_list):
 
 def map_config(max_value, canvas_width='800px', canvas_height='500px',
                legend_type='plain', legend_pos_top=None, tip_formats=None):
+    """地图设置
+
+    第一个参数是画单个主题和所有主题地图都需要传递的参数，
+    后面的参数供画所有主题地图使用，使得地图更好看。
+
+    Args:
+        max_value: 热度最高值
+        canvas_width: 画布的宽度
+        canvas_height: 画布的高度
+        legend_type: 图例的类型，普通为'plain'，多图例时用'scroll'
+        legend_pos_top: 图例距容器上方的距离
+        tip_formats: 提示框的格式
+
+    Returns:
+        china_map: 地图
+
+    """
     china_map = (
         Map(init_opts=opts.InitOpts(width=canvas_width, height=canvas_height,
                                     page_title='事件地区分布热度图',
@@ -85,13 +134,34 @@ def map_config(max_value, canvas_width='800px', canvas_height='500px',
 
 
 def merge_all_dict(dict_list):
-    # reduce()函数会对参数序列中元素进行累积
-    # 用传给reduce中的函数function（有两个参数）先对集合中的第 1、2 个元素进行操作
-    # 得到的结果再与第三个数据用function函数运算，最后得到一个结果
+    """合并所有字典，相同键的值相加。
+
+    reduce()函数会对参数序列中元素进行累积。
+    用传给reduce中的函数function（有两个参数）先对集合中的第 1、2 个元素进行操作，
+    得到的结果再与第三个数据用function函数运算，最后得到一个结果。
+    注意：最后合并的字典是直接修改在列表中第一个字典，如果要保留原来的，需要提前深拷贝再替换掉。
+
+    Args:
+        dict_list: 字典列表
+
+    Returns:
+        dict: 合并后的字典
+    """
     return reduce(merge, dict_list)
 
 
 def merge(dict1, dict2):
+    """合并两个字典，相同键的值相加。
+
+    注意：这里合并后结果直接存在dict1中，因此dict1被修改，如果要保留原来的，需要提前深拷贝。
+
+    Args:
+        dict1: 字典1
+        dict2: 字典2
+
+    Returns:
+        dict1: 合并后的字典
+    """
     for i, j in dict2.items():
         if i in dict1.keys():
             dict1[i] += j
--- src/EventsDistribution/get_events_distribution.py
+++ src/EventsDistribution/get_events_distribution.py
@@ -9,6 +9,18 @@ seach_url = 'http://xzqh.mca.gov.cn/fuzzySearch'
 
 
 def get_location(topic_id):
+    """根据主题id，获得对应主题下每个事件的发生省份。
+
+    每个事件选取第一个文档作为代表，找到对应文档的实体文件，
+    获取其中的地点实体，然后据此获取省份信息。
+    注：没有地点实体或无法通过地点实体找到省份的直接跳过。
+
+    Args:
+        topic_id: 主题id
+
+    Returns:
+        province_dict: {省份:发送次数}字典
+    """
     province_dict = {}
 
     topic_dir = os.path.join(base_dir, 'topic' + str(topic_id))
@@ -39,12 +51,28 @@ def get_location(topic_id):
 
 
 def find_province(locations):
+    """调用库从地点实体获得省份信息
+
+    Args:
+        locations: 地点实体列表
+
+    Returns:
+        provinces: 省份全称列表
+    """
     df = cpca.transform(locations)
     provinces = df['省'].tolist()
     return provinces
 
 
 def get_short_name(province):
+    """去掉省份中的后缀名，如“省”、“市”等，仅保留名称。
+
+    Args:
+        province: 省份全称列表
+
+    Returns:
+        province: 省份名称列表
+    """
     if '省' in province:
         return province[:-1]
     if '市' in province:
@@ -62,6 +90,16 @@ def get_short_name(province):
 
 
 def get_loc_entity(topic_id, doc_id, entity_dir):
+    """从实体文件中读入标签为LOC的名词作为地点实体。
+
+    Args:
+        topic_id: 主题id
+        doc_id: 文档id
+        entity_dir: 实体文件夹的路径
+
+    Returns:
+        locations: 地点实体列表
+    """
     entity_filename = 'topic' + str(topic_id) + '_entity' + str(doc_id) + '.txt'
     entity_path = os.path.join(entity_dir, entity_filename)
     with open(entity_path, 'r', encoding='utf-8') as f:
@@ -75,7 +113,7 @@ def get_loc_entity(topic_id, doc_id, entity_dir):
 
 
 def sort_key(s):
-    """ 获得‘event1’里的数字"""
+    """获得‘event1’里的数字"""
     if s:
         try:
             c = re.findall('\\d+', s)[0]
